0092aac3ad8803953f3bbb092047f3af1833f0b8
roger.santos <roger.santos@alliedbrasil.com.br>
2024-07-17 10:44:46 -0300

    Ajusta sequelize para a nova assinatura

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize';
-const sequelize = new Sequelize('sqlite::memory:', {
-    logging: false
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
 });
 
 export default sequelize;
