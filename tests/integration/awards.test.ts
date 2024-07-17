d2312738e3a4c66336a0a9eac072b43e1c0660c0
roger.santos <roger.santos@alliedbrasil.com.br>
2024-07-17 10:43:14 -0300

    Ajusta teste para ser mais específico e assertivo

--- tests/integration/awards.test.ts
+++ tests/integration/awards.test.ts
@@ -1,34 +1,32 @@
 import request from 'supertest';
 import { app, init } from '../../src/server';
 
-
 beforeAll(async () => {
-  await init(); // Inicializa o banco de dados e outras configurações
-  const PORT = process.env.PORT || 3001;
-  app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-  });
+  await init();
 });
 
 describe('GET /api/producers', () => {
-  it('should return a list of producers awards', async () => {
-    const response = await request(app).get('/api/producers');
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('min');
-    expect(response.body).toHaveProperty('max');
-    expect(Array.isArray(response.body.min)).toBe(true);
-    expect(Array.isArray(response.body.max)).toBe(true);
-    response.body.min.forEach((item: any) => {
-      expect(item).toHaveProperty('producer');
-      expect(item).toHaveProperty('interval');
-      expect(item).toHaveProperty('previousWin');
-      expect(item).toHaveProperty('followingWin');
-    });
-    response.body.max.forEach((item: any) => {
-      expect(item).toHaveProperty('producer');
-      expect(item).toHaveProperty('interval');
-      expect(item).toHaveProperty('previousWin');
-      expect(item).toHaveProperty('followingWin');
-    });
+  test('should return a list of producers awards', async () => {
+      const response = await request(app).get('/api/producers');
+      const expectedResponse = {
+        min: [
+          {
+            producer: 'Joel Silver',
+            interval: 1,
+            previousWin: 1990,
+            followingWin: 1991
+          }
+        ],
+        max: [
+          {
+            producer: 'Matthew Vaughn',
+            interval: 13,
+            previousWin: 2002,
+            followingWin: 2015
+          }
+        ]
+      };
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(expectedResponse);
   });
 });
